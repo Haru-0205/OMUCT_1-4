dbc58711466122e9bddad204b6a19f17936becfe
haru <rm23081b@osaka-pct.ac.jp>
2023-07-19 18:01:02 +0900

    update exam2.html, exam2.js and exam2.json

--- exam2.js
+++ exam2.js
@@ -1,5 +1,4 @@
 var json_data;
-var subject_array_no;
 fetch("./exam2.json")
 	.then(function (response) {
 		return response.json();
@@ -7,7 +6,6 @@ fetch("./exam2.json")
 	.then(function (json) {
 		// jsonにJSONオブジェクトで結果が渡される
 		json_data = json;
-		alert("成功");
 	})
 	.catch(function (error) {
 		alert("JSONファイルの取得に失敗しました");
@@ -16,36 +14,95 @@ fetch("./exam2.json")
 function data(subject_no) {
 	document.getElementById("subject").innerText = json_data[subject_no]["subject_jp"];
 	document.getElementById("date").innerText = json_data[subject_no]["date"];
-	get_array2("text", "range", "text", "range");
-	function get_array2(c, p, p2, id) {
-		let property_text = json_data[subject_no][c];
-		for (var i = 0; i < json_data[subject_no][p][p2]["length"]; i++) {
-			property_text += "<li>" + json_data[subject_no][p][p2][i] + "</li>"; // = ではなく += を使う
+	function get_array2(contents_name, element, element2, id) {
+		if (
+			json_data[subject_no][contents_name] != null &&
+			json_data[subject_no][element][element2] != null &&
+			id != null
+		) {
+			let property_text = json_data[subject_no][contents_name];
+			for (var i = 0; i < json_data[subject_no][element][element2]["length"]; i++) {
+				property_text += "<li>" + json_data[subject_no][element][element2][i] + "</li>"; // = ではなく += を使う
+			}
+			document.getElementById(id).innerHTML = property_text;
+		} else {
 		}
-		document.getElementById(id).innerHTML = property_text;
 	}
 
-	if (json_data[subject_no]["work1"] != null) {
+	if (typeof json_data[subject_no]["text"] === "undefined") {
+		disappear("range");
+	} else if (typeof json_data[subject_no]["range"]["text"] === "undefined") {
+		disappear("range");
+	} else {
+		appear("range");
+		get_array2("text", "range", "text", "range");
+	}
+
+	if (typeof json_data[subject_no].work1 === "undefined") {
+		disappear("work1");
+	} else {
 		document.getElementById("work1").style.display = "block";
 		get_array2("work1", "range", "work1", "work1");
-	} else {
-		document.getElementById("work1").style.display = "none";
 	}
-	if (json_data[subject_no]["work2"] != null) {
+	if (typeof json_data[subject_no].work2 === "undefined") {
+		disappear("work2");
+	} else {
 		document.getElementById("work2").style.display = "block";
 		get_array2("work2", "range", "work2", "range2");
-	} else {
-		document.getElementById("work2").style.display = "none";
 	}
 
 	if (json_data[subject_no].bring == null) {
-		document.getElementById("bring-midashi").style.display = "none";
-		document.getElementById("bring").style.display = "none";
+		disappear("bring-midashi");
+		disappear("bring");
 	} else {
 		document.getElementById("bring-midashi").style.display = "block";
 		document.getElementById("bring").style.display = "block";
 		document.getElementById("bring").innerText = json_data[no][bring];
 	}
-	get_array2("work1", "homework", "work1", "hw1");
+
+	if (typeof json_data[subject_no].homework === "undefined") {
+		disappear("hw1");
+		disappear("hw2");
+		disappear("hw3");
+		disappear("hw4");
+	} else {
+		if (typeof json_data[subject_no].homework.work1 === "undefined") {
+			disappear("hw1");
+		} else {
+			appear("hw1");
+			get_array2("work1", "homework", "work1", "hw1");
+		}
+		if (typeof json_data[subject_no]["homework"]["work2"] === "undefined") {
+			disappear("hw2");
+		} else {
+			appear("hw2");
+			get_array2("work2", "homework", "work2", "hw2");
+		}
+		if (typeof json_data[subject_no]["homework"]["other1"] === "undefined") {
+			disappear("hw3");
+		} else {
+			appear("hw3");
+			get_array2("other1", "homework", "other1", "hw3");
+		}
+		if (typeof json_data[subject_no]["homework"]["other2"] === "undefined") {
+			disappear("hw4");
+		} else {
+			appear("hw4");
+			get_array2("other2", "homework", "other2", "hw4");
+		}
+	}
+	if (typeof json_data[subject_no]["date"] === "undefined") {
+		disappear("date");
+	} else {
+		appear("date");
+	}
+
 	return subject_no;
+}
+
+function disappear(target) {
+	document.getElementById(target).style.display = "none";
+}
+function appear(target) {
+	document.getElementById(target).style.display = "inline";
 }
